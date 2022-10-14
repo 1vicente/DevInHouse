d751a47269377974b2b2c79887f34bd3d3bd4991
Vicente Siegel <visiegel1@gmail.com>
2022-10-14 13:02:22 -0300

    M1S02E03

---
+++ Modulo_1/Semana_2/Exercio_semana/m1s02e03/assets/scripts/main.js
@@ -0,0 +1,27 @@
+function onlyNumberKey(evt) {
+
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return controle = 0
+}
+
+function calculaTabuada() {
+    var numeroTabuada = parseInt(document.getElementById("numeroTabuadaHTML").value)
+    var resultado = document.getElementById("retornoJs")
+    var resultado2 = document.getElementById("retornoJs2")
+    resultado.innerHTML = ""
+    resultado2.innerHTML = ""
+
+    while (controle < 6) {
+        var multiplicacao = numeroTabuada * controle
+        resultado.innerHTML += numeroTabuada + "X" + controle + "=" + multiplicacao + "<br>"; 
+        controle ++ 
+    }
+    while (controle < 11) {
+        var multiplicacao = numeroTabuada * controle
+        resultado2.innerHTML += numeroTabuada + "X" + controle + "=" + multiplicacao + "<br>"; 
+        controle ++
+    }
+}
--- Modulo_1/Semana_2/Exercio_semana/readme.md
+++ Modulo_1/Semana_2/Exercio_semana/readme.md
@@ -1,3 +1,3 @@
 # Semana 2 DiH
 
-Aqui será postado e documentado os exercícios semanais e se possível os exercícios de cada aula.
+Aqui será postado e documentado os exercícios semanais.
