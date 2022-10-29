113f4a58a726bcf4054e35ab1a8209a85e91d2cd
Vicente Siegel <visiegel1@gmail.com>
2022-10-29 09:46:14 -0300

    M1S04E05

--- Modulo_1/Semana_4/Exercicio_semana/assets/scripts/main.js
+++ Modulo_1/Semana_4/Exercicio_semana/assets/scripts/main.js
@@ -1,5 +1,4 @@
 
-
 const recebeDados = () => {
     let resultado = document.getElementById("retornoJs")
     let cadastroClientesTemp = [{}]
@@ -16,35 +15,12 @@ const recebeDados = () => {
     resultado.innerHTML = `Sua conta foi criada com o ID ${cadastroClientesTemp.id}`
 }
 
-
-
 let cadastroClientes = [{}]
 
-const botao = document.getElementById('enviar')
+const botao = document.getElementById('enviar-cadastro')
 botao.addEventListener('click', recebeDados)
 
-// function procuraMinMax() {
+const valorBloqueado = document.getElementById('valorHtml')
 
-//     let recebeValores = pegaSelecionado()
-//     let resultado = document.getElementById("retornoJs")
-
-//     recebeValores = recebeValores.sort()
-//     let tamanhoArray = recebeValores.length
-
-//     if (isNaN(recebeValores[0]) || recebeValores[0] == "" ) {
-//         resultado.innerHTML = "O campo selecionado não é um valor válido. Selecione outro"
-//     } else {
-//     resultado.innerHTML = `Mínimo: ${recebeValores[0]} <br> Máximo: ${recebeValores[tamanhoArray - 1]} <br> Array ordenado [${recebeValores}]`
-//     }
-// }
-
-// function pegaSelecionado () {
-
-//     var radios = document.getElementsByName("escolha");
-//     for (var i = 0; i < radios.length; i++) {
-//         if (radios[i].checked) {
-//             let retorno = radios[i].value.split(",").map(Number)
-//             return retorno;
-//         }
-//     }
-// }
+const operacao = document.getElementById('operacaoHtml')
+operacao.addEventListener('change', (event) => event.target.value == "consulta" ? valorBloqueado.disabled = true :  valorBloqueado.disabled = false) 
