9f20f8c03591d35dff7f9bcc54a1618459bf00b3
Vicente Siegel <visiegel1@gmail.com>
2022-11-25 20:22:53 -0300

    M1S08E07

--- Modulo_1/Semana_8/Semana8/src/app/componentes/notification-list/notification-list.component.ts
+++ Modulo_1/Semana_8/Semana8/src/app/componentes/notification-list/notification-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
   styleUrls: ['./notification-list.component.scss']
 })
 export class NotificationListComponent {
-
+  notificacoes = [{
+    titulo: 'teste1',
+    subtitulo: 'subtitulo1',
+    data: 'data1'
+  },
+  {
+  titulo: 'teste2',
+  subtitulo: 'subtitulo2',
+  data: 'data2'
+  },
+  {
+    titulo: 'teste3',
+  subtitulo: 'subtitulo3',
+  data: 'data3'
+}]
 }
