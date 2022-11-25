99d201692afc1adfa04d9bbc7ea0f65fc1d31085
Vicente Siegel <visiegel1@gmail.com>
2022-11-25 19:46:56 -0300

    M1S08E04

--- Modulo_1/Semana_8/Semana8/src/app/app.module.ts
+++ Modulo_1/Semana_8/Semana8/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotificationListComponent } from './componentes/notification-list/notification-list.component';
+import { MenuNavbarComponent } from './componentes/menu-navbar/menu-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotificationListComponent
+    NotificationListComponent,
+    MenuNavbarComponent
   ],
   imports: [
     BrowserModule
