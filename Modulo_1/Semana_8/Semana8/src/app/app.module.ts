91b1f1964c6e9c5cb591ac025c0830de4e2be431
Vicente Siegel <visiegel1@gmail.com>
2022-11-25 19:37:40 -0300

    M1S08E03

--- Modulo_1/Semana_8/Semana8/src/app/app.module.ts
+++ Modulo_1/Semana_8/Semana8/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotificationListComponent } from './componentes/notification-list/notification-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationListComponent
   ],
   imports: [
     BrowserModule
---
+++ Modulo_1/Semana_8/Semana8/src/app/componentes/notification-list/notification-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ntap-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+
+}
