3f2a52a92d952984ad1ded30b5972f81bf6d9a75
Vicente Siegel <visiegel1@gmail.com>
2022-12-03 08:02:25 -0300

    M1S09E06

--- Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/app.component.ts
+++ Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Semana9';
+
+  tituloPersonagem: string = '@Input Personagem';
+  imagemPersonagem: string = 'https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg';
+  atorPersonagem: string = '@Input Nome do Ator';
+  descPersonagem: string = '@Input Descrição do ator';
+
+
 }
--- Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/componentes/cards/cards.component.ts
+++ Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/componentes/cards/cards.component.ts
@@ -12,11 +12,9 @@ export class CardsComponent {
   // atorPersonagem: string = "Nome do ator que faz o personagem";
   // descPersonagem: string = "Descrição do personagem";
 
-  personagem: any = {
-    nome: 'Jon Snow',
-    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
-    nomeAtor: 'Kit Harington',
-    descricao:
-      'Ator ator britânico. É mais conhecido por interpretar Jon Snow na série televisiva Game of Thrones da HBO',
-  };
+  @Input() tituloPersonagem: string = '';
+  @Input() imagemPersonagem: string = '';
+  @Input() atorPersonagem: string = '';
+  @Input() descPersonagem: string = '';
+
 }
