cf9b310f1911ac930b3513bbec685a935058047f
Vicente Siegel <visiegel1@gmail.com>
2022-12-03 07:53:00 -0300

    M1S09E05

--- Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/componentes/cards/cards.component.ts
+++ Modulo_1/Semana_9/Exercicio_semanal/Semana9/src/app/componentes/cards/cards.component.ts
@@ -6,9 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-  tituloPersonagem: string = "Personagem";
-  imagemPersonagem: string = "https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg";
-  atorPersonagem: string = "Nome do ator que faz o personagem";
-  descPersonagem: string = "Descrição do personagem";
 
+  // tituloPersonagem: string = "Personagem";
+  // imagemPersonagem: string = "https://i.pinimg.com/originals/e4/34/2a/e4342a4e0e968344b75cf50cf1936c09.jpg";
+  // atorPersonagem: string = "Nome do ator que faz o personagem";
+  // descPersonagem: string = "Descrição do personagem";
+
+  personagem: any = {
+    nome: 'Jon Snow',
+    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Ator ator britânico. É mais conhecido por interpretar Jon Snow na série televisiva Game of Thrones da HBO',
+  };
 }
