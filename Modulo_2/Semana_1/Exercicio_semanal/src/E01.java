aac8bfa9cd5af6a7621bde2d61b63ada726418b1
Vicente Siegel Neto <vicente_siegel@estudante.sc.senai.br>
2023-02-09 20:38:08 -0300

    m2s02e01

---
+++ Modulo_2/Semana_1/Exercicio_semanal/src/E01.java
@@ -0,0 +1,11 @@
+public class E01 {
+    public static void main(String[] args) {
+
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        System.out.println("Primeiro valor: " + arr[0]);
+        System.out.println("Último valor: " + arr[arr.length-1]);
+        System.out.println("Terceiro valor: " + arr[2]);
+
+    }
+}
