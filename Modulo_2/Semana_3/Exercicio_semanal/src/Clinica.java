3c5c78c3f818ad1c96772d9e009e0849be249af3
Vicente Siegel Neto <vicente_siegel@estudante.sc.senai.br>
2023-02-17 18:27:02 -0300

    m02s03e01

---
+++ Modulo_2/Semana_3/Exercicio_semanal/src/Clinica.java
@@ -0,0 +1,38 @@
+public class Clinica {
+    private String nome;
+    private String cnpj;
+    private String logradouro;
+    private String codigo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ Modulo_2/Semana_3/Exercicio_semanal/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Clinica clinicaMain = new Clinica();
+
+        System.out.println("Cadastro da Clínica");
+        System.out.printf("CNPJ.........:");
+        clinicaMain.setCnpj(scanner.next());
+        System.out.printf("Nome.........:");
+        scanner.nextLine();
+        clinicaMain.setNome(scanner.nextLine());
+        System.out.printf("Código.......:");
+        clinicaMain.setCodigo(scanner.next());
+        System.out.printf("Logradouro...:");
+        scanner.nextLine();
+        clinicaMain.setLogradouro(scanner.nextLine());
+
+        System.out.println(clinicaMain.getLogradouro());
+    }
+
+
+}
